bc1a544fed1ebdb86fb89d29ea07408cc0c74b59
Jurkowski, Gustaw <gustaw.jurkowski@misys.com>
2018-11-06 15:06:39 +0100

    Display individual jobs

--- jobs/app.py
+++ jobs/app.py
@@ -35,6 +35,12 @@ def execute_sql(sql, values=(), commit=False, single=False):
 
 @app.route('/')
 @app.route('/jobs')
-def jobs():    
+def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
+
+
+@app.route('/job/<job_id>')
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    return render_template('job.html', job=job)
